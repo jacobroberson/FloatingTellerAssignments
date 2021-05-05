d653b08dcf759c199dfbffa1dece6422455b1542
jacobroberson <60160656+jacobroberson@users.noreply.github.com>
2021-05-05 17:21:12 -0500

    Write BranchInputs method and initialize variables

--- FloatingTellerAssignments/src/main/java/FloatingTellerAssignments.java
+++ FloatingTellerAssignments/src/main/java/FloatingTellerAssignments.java
@@ -13,11 +13,19 @@ public class FloatingTellerAssignments {
 	// Initialize variables
 	// Number of Floating Tellers available
 	int NumberOfFloatingTellers = 0;
+	// Number of Branches
+	int NumberOfBranches = 0;
 	// An integer to be used in loops and for tracking indices
 	int i = 0;
-	
+
 	// Stores the names of the Floating Tellers
 	List<String> FloatingTellerNames;
+	// Stores the names of the Branches
+	List<String> BranchNames;
+	// Stores the number of average customers for each branch
+	int[] BranchCustomers;
+	// Stores the number of tellers already working at each branch
+	int[] BranchTellers;
 
 	// Introduction Script that runs at the start of the program
 	public void IntroScript() {
@@ -32,8 +40,9 @@ public class FloatingTellerAssignments {
 	public void FloatingTellerInput() {
 		// Receive input from user on the number of available floating tellers
 		System.out.println("Please enter the number of Floating Tellers available on this day: ");
-			NumberOfFloatingTellers = Integer.parseInt(sc.nextLine());
-			FloatingTellerNames = new ArrayList<String>(NumberOfFloatingTellers);
+		NumberOfFloatingTellers = Integer.parseInt(sc.nextLine());
+		FloatingTellerNames = new ArrayList<String>(NumberOfFloatingTellers);
+
 		// Receive input from user on the names of the floating tellers
 		for (i = 0; i < NumberOfFloatingTellers; i++) {
 			System.out.println("Please enter a name to assign to Floating Teller " + (i + 1) + ": ");
@@ -49,7 +58,32 @@ public class FloatingTellerAssignments {
 
 	// Collects user input regarding branches
 	public void BranchesInput() {
+		// Receive input from user on the number of Branches
+		System.out.println("\nPlease enter the number of branch locations: ");
+		NumberOfBranches = Integer.parseInt(sc.nextLine());
+		BranchNames = new ArrayList<String>(NumberOfBranches);
+		BranchCustomers = new int[NumberOfBranches];
+		BranchTellers = new int[NumberOfBranches];
 
+		// Receive input from user on the information for the branches
+		for (i = 0; i < NumberOfBranches; i++) {
+			System.out.println("Please enter a name to assign to Branch " + (i + 1) + ": ");
+			BranchNames.add(sc.nextLine());
+			System.out.println("How many customers does " + BranchNames.get(i)
+					+ " have on average each day? (Please enter a whole number)");
+			BranchCustomers[i] = Integer.parseInt(sc.nextLine());
+			System.out.println("How many tellers does " + BranchNames.get(i)
+					+ " have working on this day? (Please enter a whole number)");
+			BranchTellers[i] = Integer.parseInt(sc.nextLine());
+		}
+
+		// Display the data entered
+		System.out.println("\nData Entered:");
+		for (i = 0; i < NumberOfBranches; i++) {
+			System.out.println("Branch " + (i + 1) + " is: " + BranchNames.get(i) + "\n" + BranchNames.get(i) + " has "
+					+ BranchCustomers[i] + " customers on average each day.\n" + BranchNames.get(i) + " has "
+					+ BranchTellers[i] + " tellers on this day.");
+		}
 	}
 
 	// Calculates the average number of customers per teller for each branch and
