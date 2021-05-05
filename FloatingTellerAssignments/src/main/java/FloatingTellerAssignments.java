ff445eea41417f9ddcea99617c565b3b2904e946
jacobroberson <60160656+jacobroberson@users.noreply.github.com>
2021-05-05 17:10:48 -0500

    Set up methods

--- FloatingTellerAssignments/src/main/java/FloatingTellerAssignments.java
+++ FloatingTellerAssignments/src/main/java/FloatingTellerAssignments.java
@@ -1,4 +1,37 @@
-
+// Programmed by Jacob Roberson
+/**
+ * @author Jacob Roberson
+ * This program is used to assign floating tellers to branches based on need.
+ */
 public class FloatingTellerAssignments {
-
+	
+	// Introduction Script that runs at the start of the program
+	public void IntroScript() {
+	
+	}
+	
+	// Collects user input regarding floating tellers
+	public void FloatingTellerInput() {
+		
+	}
+	
+	// Collects user input regarding branches
+	public void BranchesInput() {
+		
+	}
+	
+	// Calculates the average number of customers per teller for each branch and assigns floating tellers
+	public void MakeAssignments() {
+		
+	}
+	
+	// Main Method
+	public static void main(String[] args) {
+		FloatingTellerAssignments fta = new FloatingTellerAssignments();
+		fta.IntroScript();
+		fta.FloatingTellerInput();
+		fta.BranchesInput();
+		fta.MakeAssignments();
+	}
 }
+
